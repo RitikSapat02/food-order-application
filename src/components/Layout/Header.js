828c88f40ee4dce58c5ac74b68ef5e25af866184
Ritik <ritiksapat@gmail.com>
2023-06-28 15:30:48 +0530

    adding cart

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import mealsImag from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCardButton from "./HeaderCardButton";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <HeaderCardButton />
+      </header>
+
+      <div>
+        <img
+          src={mealsImag}
+          className={classes["main-image"]}
+          alt="A table full of delicious food"
+        />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,20 @@
+import classes from "./MealItem.module.css";
+import MealsItemForm from "./MealItemForm";
+
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.desc}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealsItemForm />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
